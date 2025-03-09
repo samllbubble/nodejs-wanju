9a01c5da70d4af7b183da6aa09a4a196b76a7e2e
samllbubble <139466261+samllbubble@users.noreply.github.com>
2025-03-09 19:21:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const port = process.env.PORT || process.env.SERVER_PORT || 3000;
-const { spawn } = require('child_process');
+const FILE_PATH = process.env.FILE_PATH || './.npm';
+const http = require('http');
 const fs = require('fs');
+const { spawn } = require('child_process');
 
 const startScriptPath = './start.sh';
-fs.chmodSync(startScriptPath, 0o755);
+// fs.chmodSync(startScriptPath, 0o755);
+const subFilePath = FILE_PATH + '/log.txt';
 
 const startScript = spawn(startScriptPath);
 startScript.stdout.on('data', (data) => {
@@ -17,4 +20,29 @@ startScript.on('error', (error) => {
   process.exit(1);
 });
 
-console.log(`server is listening on port : ${port}`);
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200);
+    res.end('hello world');
+  } else if (req.url === '/healthcheck') {
+    res.writeHead(200);
+    res.end('ok');
+  } else if (req.url === '/sub') {
+    fs.readFile(subFilePath, 'utf8', (error, data) => {
+      if (error) {
+        res.writeHead(500);
+        res.end('Error reading file');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Not found');
+  }
+});
+
+server.listen(port, () => {
+  console.log(`server is listening on port : ${port}`);
+});
